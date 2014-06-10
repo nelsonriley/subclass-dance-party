66831e8a88376d0b2e4462ed6994e36801b601bc
nelsonriley <nelsonriley@gmail.com>
2014-06-09 18:10:36 -0700

    Has a step function that makes the node blink

--- src/dancer.js
+++ src/dancer.js
@@ -1,11 +1,39 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps){
+  // use jQuery to create an HTML <span> tag
+  this.timeBetweenSteps = timeBetweenSteps;
 
+  this.$node = $('<span class="this"></span>');//
+  this.step();
+  // now that we have defined the this object, we can start setting up important parts of it by calling the methods we wrote
+  // this one sets the position to some random default point within the body
+  this.setPosition(top, left);
+};
+
+makeDancer.prototype.step = function(){
+  // the basic this doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  //var timey = setTimeout.bind(this);
+  //timey(this.step, this.timeBetweenSteps);
+  setTimeout(this.step, this.timeBetween);
+};
+
+makeDancer.prototype.setPosition = function(top, left){
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
+/*
   var dancer = {};
+
 
   // use jQuery to create an HTML <span> tag
   dancer.$node = $('<span class="dancer"></span>');
-
 
   dancer.step = function(){
     // the basic dancer doesn't do anything interesting at all on each step,
@@ -31,3 +59,5 @@ var makeDancer = function(top, left, timeBetweenSteps){
 
   return dancer;
 };
+
+*/
